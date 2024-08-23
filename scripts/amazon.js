c6a775e489f3cf9f1e6c9752a6cd1284b05117d2
Praise1214 <agapepraiz@gmail.com>
2024-08-23 18:08:15 +0100

    Made the cartQuantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -63,23 +63,29 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {button.addEventListener('click', () => {
   const productId = button.dataset.productId;
 
-  let matchingItem;
-  cart.forEach((item) => {
-    if(productId === item.productId){
-      matchingItem = item;
-    };
-  });
+        let matchingItem;
+        cart.forEach((item) => {
+          if(productId === item.productId){
+            matchingItem = item;
+          };
+        });
 
-  if (matchingItem) {
-    matchingItem.quantity += 1;
-  }
-  else {
-    cart.push({
-      productId: productId,
-      quantity: 1,
-    })
+        if (matchingItem) {
+          matchingItem.quantity += 1;
+        }
+        else {
+          cart.push({
+            productId: productId,
+            quantity: 1,
+          })
+        }
 
-  }
-  console.log(cart)
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
+        })
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
 })
 })
