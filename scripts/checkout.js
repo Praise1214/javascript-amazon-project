07613408972046d80b7abd6841f57fcd6e96bfbf
Praise1214 <agapepraiz@gmail.com>
2024-08-28 01:31:21 +0100

    delete product from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach((cartItem) => {
 
 console.log(matchingProduct);
 
-cartSummaryHTML += `  <div class="cart-item-container">
+cartSummaryHTML += `  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Wednesday, June 15
         </div>
@@ -100,7 +100,10 @@ document.querySelectorAll('.js-delete-link')
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
             removeFromCart(productId);
-            console.log(cart)
+            
+            const container = document.querySelector(`.js-cart-item-container-${productId}`)
+
+           container.remove()
         }
          )
     })
