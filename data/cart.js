b657a6a012366e6cfbd010cfd6f41d914ef5237a
Praise1214 <agapepraiz@gmail.com>
2024-08-28 01:42:11 +0100

    saved cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,22 @@
-export let cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-}, {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
+export let cart = JSON.parse(localStorage.getItem('cart')) 
+
+if(!cart) {
+    cart = [{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+    }, {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+    }
+    
+    ]
 }
 
-]
+
+function savetoStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
 
 export function addtoCart(productId, quantity) {
     let matchingItem;
@@ -25,6 +35,8 @@ export function addtoCart(productId, quantity) {
           quantity,
         })
       }
+
+      savetoStorage
   }
 
   export function removeFromCart(productId) {
@@ -37,4 +49,6 @@ export function addtoCart(productId, quantity) {
      })
 
      cart = newCart;
+
+     savetoStorage;
   }
