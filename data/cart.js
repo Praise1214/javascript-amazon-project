0141a7a99a81aa1e129efb526334bd5dea5d53b4
Praise1214 <agapepraiz@gmail.com>
2024-09-17 10:34:04 +0100

    Created Automated test for addtoCart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,9 @@
-export let cart = JSON.parse(localStorage.getItem('cart'))
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'))
 
 if(!cart) {
     cart = [{
@@ -14,7 +19,7 @@ if(!cart) {
     ];
 }
 
-
+}
 export function savetoStorage() {
     localStorage.setItem('cart', JSON.stringify(cart));
 }
