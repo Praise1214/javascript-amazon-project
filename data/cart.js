efa8b9afb8fb90826f928f86116e0e123dc09c94
Praise1214 <agapepraiz@gmail.com>
2024-09-28 23:06:24 +0100

    learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -104,6 +104,21 @@ export function addtoCart(productId) {
     return cartQuantity
   }
 
+  export function loadCart(fun) {
+    const xhr = new XMLHttpRequest();
+  
+    xhr.addEventListener('load', () => {
+      console.log(xhr.response);
+      fun();
+    })
+  
+    xhr.open('GET', 'https://supersimplebackend.dev/cart')
+    xhr.send();
+  }
+  
+  
+  
+
 
   
   
